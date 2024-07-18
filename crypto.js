44a5c128448e231aca263dd2e5aebf72f5a00262
DyagilevDS <denis.panik@gmail.com>
2024-07-18 19:05:35 +0300

    crypto

--- crypto.js
+++ crypto.js
@@ -1,14 +1,11 @@
 function generateRandomString() {
-  let length = 32;
   let result = '';
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let length = 32;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
+  const array = new Uint8Array(length);
+  crypto.getRandomValues(array);
+  array.forEach(x => result += characters[x % charactersLength]);
   return result;
 }
 
